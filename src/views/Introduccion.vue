59d0ca7d0d6d93bad9271f5bf6588f0c22f10817
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-05-23 15:20:46 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p.mb-5 La investigación científica está basada en métodos que son determinantes para la estructuración de una tesis, vea a continuación en qué consiste:
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/DvYrv4kCFI8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/DvYrv4kCFI8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)  
 
 </template>
 
